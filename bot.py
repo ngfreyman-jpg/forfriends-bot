b9860114e6a522f13541c65e8750f6102f468b75
ngfreyman-jpg <ngfreyman@gmail.com>
2025-10-21 18:27:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
-import os
-import asyncio
+import asyncio, os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN)
+TOKEN = os.getenv("BOT_TOKEN")  # возьмём из переменных окружения на Render
+
+bot = Bot(TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def start_cmd(message: types.Message):
-    await message.answer("Привет! Бот работает ✅")
+async def start_cmd(m: types.Message):
+    await m.answer("Привет! Бот работает ✅")
 
 async def main():
     await dp.start_polling(bot)
